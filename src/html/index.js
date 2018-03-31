3e731539bddbff06ac1edd3d32a82ace3d6a2e55
gvergnaud <gabriel.vergnaud@hetic.net>
2018-03-31 18:26:06 +0200

    Allow having several elements at the top level of the html tag

--- src/html/index.js
+++ src/html/index.js
@@ -3,13 +3,26 @@ import h, { VNode } from './h'
 import patch, { createElement } from './patch'
 import Observable from '../Observable'
 import { createReactiveTag } from '../core'
+import { dropRight, last } from '../utils'
 
 const hx = hyperx(h, { attrToProp: false })
 
 // type Tag a b = [String] -> ...[a] -> b
 
+const addWrappingTag = ([x, ...strings]) => [
+  `<div>${x}`,
+  ...dropRight(1, strings),
+  `${last(strings)}</div>`
+]
+
+const removeWrappingTag = ({ children }) => {
+  const childElements = children.filter(c => c instanceof VNode)
+  return childElements.length === 1 ? childElements[0] : childElements
+}
+
 // htmlTag :: Tag a VirtualDOM
-const htmlTag = hx
+const htmlTag = (strings, ...variables) =>
+  removeWrappingTag(hx(addWrappingTag(strings), ...variables))
 
 // render :: Observable VirtualDOM -> DOMElement -> Promise Error ()
 const render = (component, element) => {
