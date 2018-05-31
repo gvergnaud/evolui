44cab83e92f6468596270513b5adea4942d2666c
gvergnaud <gabriel.vergnaud@gmail.com>
2018-05-31 19:17:49 +0200

    wip

--- example/src/index.js
+++ example/src/index.js
@@ -1,64 +1,110 @@
 import html, { render, h } from 'evolui'
 import { ease } from 'evolui/extra'
 import { merge, fromEvent } from 'rxjs'
-import { map, startWith } from 'rxjs/operators'
+import { switchMap, map, startWith, shareReplay } from 'rxjs/operators'
+
+const log = (x, label = 'log') => (console.log(label, x), x)
+
+const range = (x, y) =>
+  Array(y - x)
+    .fill(0)
+    .map((_, i) => i + x)
 
 const toPosition = e =>
   (e.position = e.type.match(/^touch/)
     ? { x: e.touches[0].clientX, y: e.touches[0].clientY }
     : { x: e.clientX, y: e.clientY })
 
-const Circle = props$ =>
-  props$.pipe(
+const mouse$ = merge(
+  fromEvent(window, 'mousemove').pipe(map(toPosition)),
+  fromEvent(window, 'touchmove').pipe(map(toPosition))
+).pipe(
+  startWith({
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2
+  }),
+  shareReplay(1)
+)
+
+const Circle = props$ => {
+  const style$ = props$.pipe(
     map(
       ({
+        key,
         color = 'purple',
         radius = 25,
         stiffness = 120,
         damping = 20
-      } = {}) => {
-        const position$ = merge(
-          fromEvent(window, 'mousemove').pipe(map(toPosition)),
-          fromEvent(window, 'touchmove').pipe(map(toPosition))
-        ).pipe(startWith({ x: 0, y: 0 }))
-
-        return (
-          <div
-            style={position$.pipe(
-              ease({
-                x: [stiffness, damping],
-                y: [stiffness, damping]
-              }),
-              map(({ x, y }) => ({
-                position: 'absolute',
-                left: '0',
-                top: '0',
-                width: radius * 2 + 'px',
-                height: radius * 2 + 'px',
-                background: color,
-                borderRadius: '100%',
-                zIndex: -1,
-                transform: `translate(${x - radius}px, ${y - radius}px)`
-              }))
-            )}
-          />
+      } = {}) =>
+        mouse$.pipe(
+          ease(
+            {
+              x: [stiffness, damping],
+              y: [stiffness, damping]
+            },
+            key
+          ),
+          map(({ x, y }) => ({
+            position: 'absolute',
+            left: '0',
+            top: '0',
+            width: radius * 2 + 'px',
+            height: radius * 2 + 'px',
+            background: color,
+            borderRadius: '100%',
+            zIndex: -1,
+            transform: `translate(${x - radius}px, ${y - radius}px)`
+          }))
         )
-      }
     )
   )
+  return <div style={style$} />
+}
 
-render(
+const Lol = () => (
+  <p
+    style={{
+      fontSize: mouse$.pipe(ease({ x: [120, 18] }), map(m => m.x / 3 + 'px'))
+    }}
+  >
+    LOL
+  </p>
+)
+
+const sub = render(
   <div>
-    {Array(50)
-      .fill(0)
-      .map((_, i) => (
+    <Lol />
+    <div>
+      {range(0, 10).map(i => (
         <Circle
+          key={i}
           stiffness={100 + i * 10}
           damping={16}
           radius={35}
-          color={`rgba(5, 241, 163, ${(i + 1) / 10})`}
+          color={`rgba(5, 241, 163, ${i / 10})`}
         />
       ))}
+    </div>
   </div>,
   document.querySelector('#root')
 )
+
+// render(
+//   <div>
+//     {range(0, 10).map(i => (
+//       <Circle
+//         stiffness={100 + i * 10}
+//         damping={16}
+//         radius={35}
+//         color={`rgba(5, 241, 163, ${(i + 1) / 10})`}
+//       />
+//     ))}
+//   </div>,
+//   document.querySelector('#root')
+// )
+
+if (module.hot) {
+  module.hot.dispose(() => {
+    sub.unsubscribe()
+  })
+}
