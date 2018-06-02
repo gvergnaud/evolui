682d3915f20b358921ab831e50ebe597ccc98dec
gvergnaud <gabriel.vergnaud@gmail.com>
2018-06-02 13:51:36 +0200

    Migrate from parcel to webpack

---
+++ example/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      evolui: path.resolve(__dirname, '../src'),
+      'evolui/extra': path.resolve(__dirname, '../src/extra')
+    }
+  }
+}
