ccfea39d09a21221936256551c8208119e95e5a3
gvergnaud <gabriel.vergnaud@gmail.com>
2018-06-11 11:20:00 +0200

    [#feature/jsx] Fixes html tag function deep-patch

--- src/html/html.js
+++ src/html/html.js
@@ -1,45 +1,7 @@
 import createTag from 'vdom-tag'
-import { Observable } from '../utils/observables'
-import { createReactiveTag } from '../core'
-import patch, { VNode, VPatch } from './patch'
 import h from './h'
 
-const createRenderProcess = vdom$ =>
-  new Observable(observer => {
-    let domNode
-    let previousTree
-    let isSvg
-
-    return vdom$.subscribe({
-      complete: () => observer.complete(),
-      error: e => observer.error(e),
-      next: vTree => {
-        if (!(vTree instanceof VNode)) return observer.next(vTree)
-
-        const onMount = vTree.lifecycle.mount
-
-        vTree.lifecycle.mount = (node, _isSvg) => {
-          domNode = node
-          isSvg = _isSvg
-          onMount(node)
-        }
-
-        if (!domNode) {
-          observer.next(vTree)
-        } else {
-          domNode = patch(domNode, previousTree, vTree, isSvg)
-          observer.next(new VPatch({ vTree }))
-        }
-
-        previousTree = vTree
-      }
-    })
-  })
-
-const toRenderProcess = tag => (strings, ...variables) =>
-  createRenderProcess(tag(strings, ...variables))
-
-// html :: [String] -> ...[Variable a] -> Observable VirtualDOM
-const html = toRenderProcess(createReactiveTag(createTag(h)))
+// html :: [String] -> ...[Variable a] -> VirtualDOM
+const html = createTag(h)
 
 export default html
